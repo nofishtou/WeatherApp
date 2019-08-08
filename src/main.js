d1958030abcad1002c6393b5d70c84b994d88b16
Andrew Zubkov <36769971+nofishtou@users.noreply.github.com>
2019-08-08 21:56:39 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,42 +10,6 @@ const windDirection = document.querySelector('.wind-direction');
 const icon = document.querySelector('.icon');
 const btns = document.querySelectorAll('.btn')
 
-// const loadData = (e) => {
-//   const xhr = new XMLHttpRequest();
-//   let townId
-//   if(e.type === 'load') {
-//     let  defaultMean = document.querySelector('.activebtn')
-//     townId = defaultMean.getAttribute('data-id')
-//   } else {
-//     townId = e.target.getAttribute('data-id');
-//   }
-
-//   xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?id=${townId}&units=metric&appid=237d46de088807b8ea4371af72fb7dbd`, true);
-
-//   xhr.onload = function() {
-//     let data = JSON.parse(this.responseText)
-//     let url = data.weather[0].icon
-//     cityName.innerHTML = data.name;
-//     temperature.innerHTML = data.main.temp
-//     humidity.innerHTML = data.main.humidity
-//     atmPresure.innerHTML = data.main.pressure
-//     windSpeed.innerHTML = data.wind.speed
-//     windDirection.innerHTML = data.wind.deg
-//     icon.innerHTML =`<img src="https://openweathermap.org/img/wn/${url}@2x.png" alt="" srcset="" width="20px" height="20px"/>`
-//   }
-  
-//   xhr.send();
-
-//   if (e.type === 'click'){
-//     btns.forEach( elem => {
-//       if(elem.className === "btn activebtn") {
-//       elem.classList.remove("activebtn")
-//       }
-//     })
-//     e.target.classList.add('activebtn')
-//   }
-// }
-
 const loadData = (e) => {
   let townId
   if(e.type === 'load') {
